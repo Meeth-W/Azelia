ecde097a7163329f89a58dd287d87a54793bcb8e
Meeth-W <meeth2111@gmail.com>
2025-01-09 16:04:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Azelia 
+
+A simple discord bot to handle ollama AI Conversations through the discord platform.
---
+++ bot.py
@@ -0,0 +1,134 @@
+import os
+import random
+import logging
+import platform
+import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+from dotenv import load_dotenv
+from functions import ConfigManager, OllamaHandler
+
+# Loading Configs
+config = ConfigManager('config.yml')
+
+# Intents 
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Logging Handlers
+class LoggingFormatter(logging.Formatter):
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
+
+    def format(self, record):
+        log_color = self.COLORS[record.levelno]
+        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format = format.replace("(black)", self.black + self.bold)
+        format = format.replace("(reset)", self.reset)
+        format = format.replace("(levelcolor)", log_color)
+        format = format.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
+        return formatter.format(record)
+
+logger: logging.Logger = logging.getLogger('discord_bot')
+logger.setLevel(logging.INFO)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LoggingFormatter())
+# File handler
+file_handler = logging.FileHandler(filename="logs/main.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
+)
+file_handler.setFormatter(file_handler_formatter)
+
+# Add the handlers
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+# Initialize OllamaHandler
+ollama_handler = OllamaHandler()
+
+# Main Bot
+class Azelia(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(config.fetch('prefix')),
+            intents=intents,
+        )
+        self.api_key = os.getenv('APIKEY')
+        self.logger = logger
+    
+    async def load_cogs(self) -> None:
+        """
+        Load bot modules
+        """
+        for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/modules"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"modules.{extension}")
+                    self.logger.info(f"Loaded extension '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    self.logger.error(f"Failed to load extension {extension}\n{exception}")
+                
+    @tasks.loop(minutes=1.0)
+    async def status_task(self) -> None:
+        """
+        Setup the game status task of the bot.
+        """
+        choices = config.fetch('status')
+        await self.change_presence(activity=discord.Game(random.choice(choices)))
+    
+    @status_task.before_loop
+    async def before_status_task(self) -> None:
+        await self.wait_until_ready()
+    
+    async def setup_hook(self) -> None:
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(
+            f"Running on: {platform.system()} {platform.release()} ({os.name})"
+        )
+        self.logger.info("-------------------")
+        await self.load_cogs()
+        self.status_task.start()
+    
+    async def on_message(self, message: discord.Message) -> None:
+        if message.author == self.user or message.author.bot:
+            return
+
+        if message.content.startswith(config.fetch('prefix')):
+            await self.process_commands(message)
+        else: 
+            channel_id = config.fetch("channel_id")
+            if message.channel.id == channel_id:
+                await message.channel.typing()
+                logger.info(f"User: {message.content}")
+                response = ollama_handler.chat(message.content)
+                if response: 
+                    await message.channel.send(response)
+                    logger.info(f"Lilly: {response}")
+
+
+load_dotenv()
+
+bot = Azelia()
+bot.run(os.getenv("TOKEN"))
